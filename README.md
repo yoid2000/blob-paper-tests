617857fb588735e3bf1ba79f1e01460721fb7248
yoid2000 <paul@francis.com>
2025-01-22 11:19:05 +0100

    initial install

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # blob-paper-tests
 Contains all of the software for the blob paper
+
+
+
+
+
+# Install
+
+pip install git+https://github.com/yoid2000/blob-paper-tests.git
+
---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='blob-paper-tests',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[],
+    author='Paul Francis',
+    author_email='francis@mpi-sws.org',
+    description='The code for my blob paper',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+    ],
+    license='GNU General Public License v3',
+)
