24fbe9ad915db0c8b8c2386a1f8252ef1d3a040e
yoid2000 <paul@francis.com>
2025-01-29 16:22:22 +0100

    updates

--- blob-paper-tests/mutual_info/compute_mutual_info.py
+++ blob-paper-tests/mutual_info/compute_mutual_info.py
@@ -88,6 +88,9 @@ def check_pairs(file_path, blob_name, blob_dir_path):
         num_unique_syn_col2 = df_syn[col2].nunique()
         num_unique_orig_col1 = df_orig[col1].nunique()
         num_unique_orig_col2 = df_orig[col2].nunique()
+        # Get the number of distinct value pairs for col1 and col2 for both df_syn and df_orig
+        num_unique_pairs_syn = df_syn[[col1, col2]].drop_duplicates().shape[0]
+        num_unique_pairs_orig = df_orig[[col1, col2]].drop_duplicates().shape[0]
         # get the column type for col1 and col2 for both df_syn and df_orig
         col1_type_syn = str(df_syn[col1].dtype)
         col2_type_syn = str(df_syn[col2].dtype)
@@ -98,14 +101,17 @@ def check_pairs(file_path, blob_name, blob_dir_path):
                         'column2':col2,
                         'mi_syn':mi_syn,
                         'mi_orig':mi_orig,
-                        'num_unique_syn_col1':num_unique_syn_col1,
-                        'num_unique_syn_col2':num_unique_syn_col2,
                         'num_unique_orig_col1':num_unique_orig_col1,
+                        'num_unique_syn_col1':num_unique_syn_col1,
                         'num_unique_orig_col2':num_unique_orig_col2,
-                        'col1_type_syn':col1_type_syn,
-                        'col2_type_syn':col2_type_syn,
+                        'num_unique_syn_col2':num_unique_syn_col2,
+                        'num_unique_pairs_orig':num_unique_pairs_orig,
+                        'num_unique_pairs_syn':num_unique_pairs_syn,
                         'col1_type_orig':col1_type_orig,
-                        'col2_type_orig':col2_type_orig})
+                        'col1_type_syn':col1_type_syn,
+                        'col2_type_orig':col2_type_orig,
+                        'col2_type_syn':col2_type_syn,
+        })
     # convert results to a dataframe
     df_results = pd.DataFrame(results)
     return df_results
