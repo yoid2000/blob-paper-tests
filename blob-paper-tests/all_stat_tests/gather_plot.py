5c9e41189fef997e9c2c691dd57625c779b2dfa3
yoid2000 <paul@francis.com>
2025-02-18 14:03:17 +0100

    Update gather_plot.py

--- blob-paper-tests/all_stat_tests/gather_plot.py
+++ blob-paper-tests/all_stat_tests/gather_plot.py
@@ -496,6 +496,13 @@ def plot_score_list_lines(df, tag):
     plt.savefig(os.path.join('real_results', f'score_list_lines_{tag}.pdf'))
     plt.close()
 
+def file_is_in_kaggle(file, kaggle_files):
+    # go through each name in kaggle_files. If file begins with name, return True
+    for name in kaggle_files:
+        if file.startswith(name):
+            return True 
+    return False
+
 blob_code_path = '.'
 # check if there is an environment variable called BLOB_TEST_DIR
 if 'BLOB_TEST_DIR' in os.environ:
@@ -507,6 +514,7 @@ else:
 print(f"Blob path: {blob_path}")
 
 results_path = os.path.join(blob_path, 'results')
+kaggle_path = os.path.join(blob_path, 'datasets', 'kaggle_parquet')
 all_results_path = os.path.join(results_path, 'all_tests')
 
 results_file_path = os.path.join(results_path, 'all_results.parquet')
@@ -518,6 +526,8 @@ if os.path.exists(results_file_path):
     # read file as df
     df = pd.read_parquet(results_file_path)
 else:
+    # read all of the base kaggle file names in kaggle_path that end with .parquet, strip off the suffix, and put in a list called kaggle_files
+    kaggle_files = [f[:-8] for f in os.listdir(kaggle_path) if f.endswith('.parquet')]
     # read all parquet files in all_results_path and sub directories
     # and gather the results in a single dataframe
     num_files_read = 0
@@ -525,6 +535,9 @@ else:
     for root, dirs, files in os.walk(all_results_path):
         for file in files:
             if file.endswith('.parquet'):
+                if not file_is_in_kaggle(file, kaggle_files):
+                    print(f"File {file} not in kaggle, skip")
+                    continue
                 file_path = os.path.join(root, file)
                 df_temp = pd.read_parquet(file_path)
                 df = pd.concat([df, df_temp])
